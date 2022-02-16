6f59783ccfd1c9177e2e379f58d9f1ca72814306
marisagn97 <92514426+marisagn97@users.noreply.github.com>
2022-02-16 20:16:16 +0100

    Add files via upload
    Puesta en producción

---
+++ request_error.py
@@ -0,0 +1,35 @@
+# Importación de librerías
+import requests
+
+# Diccionario/JSON con los datos
+request_dict = {
+    "C_YEAR": 'Pepe',
+    "C_MNTH": 8,
+    "C_WDAY": 5,
+    "C_HOUR": 14,
+    "C_WTHR": "1",
+    "C_RALN": "1",
+    "P_SEX": "M",
+    "P_SAFE": "02",
+    "P_USER": "2",
+    "C_VEHS": "2",
+    "P_AGE": "04",
+    "V_ANTIG": "19",
+    "P_PSN": "Luis",
+    "C_CONF": "31",
+    "C_TRAF": "18",
+    "V_TYPE": "01",
+    "C_RCFG": "01",
+    "C_RSUR": "1"
+}
+
+# Definición ruta /predict
+url = "http://127.0.0.1:5000/predict"
+
+# Generación de la petición de tipo POST
+r = requests.post(url, json=request_dict)
+
+# Comprobación del estado de la petición por medio de su código de estado
+r.status_code
+
+print(r.json())
